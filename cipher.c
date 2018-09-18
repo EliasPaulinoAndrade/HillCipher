0175275593ded361b9cd061aad7519015fff571c
EliasPaulinoAndrade <eliaspaulinoandrade@gmail.com>
2018-09-17 21:16:00 -0300

    simplifying code

--- cipher.c
+++ cipher.c
@@ -2,74 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-//checar ponteiro vazio sempre que usar malloc
-//usar menos mallocs
+#define GAP 2
+#define char_to_number(c) (c - 97)
+#define number_to_char(i) (i + 97)
 
-typedef struct {
-  int *mat;
-  int width;
-  int height; 
-} Matrix;
+//CRIACAO DE STRING SEM /0
 
-Matrix matrix_init(int height, int width, int *matt){
-  /*aloca matriz e retorna*/
 
-  Matrix mat;
-  mat.width = width;
-  mat.height = height;
-  
-  mat.mat = matt;
-  
-  return mat;
-}
-
-int matrix_index(int row, int col, int width){
-  return row * width + col; 
-}
-
-void matrix_show(Matrix *matrix){
+void matrix_show(int width, int height, int matrix[height][width]){
   /*printa matrix*/
 
-  printf("width: %d, height: %d\n", matrix->width, matrix->height);
+  printf("width: %d, height: %d\n", width, height);
   
   int col, row;
   
-  for(row = 0; row < matrix->height; row++) {
-    for(col = 0; col < matrix->width; col++) {
-      printf("[%d]\t", matrix->mat[ matrix_index(row, col, matrix->width) ]);
+  for(row = 0; row < height; row++) {
+    for(col = 0; col < width; col++) {
+      printf("[%d]\t", matrix[row][col]);
     }
     printf("\n");
   }
 }
-
+/*
 Matrix *matrix_adjunt(Matrix *matrix) {
-  /*calcula matrix adjunta*/
+  
   
   return NULL;
 }
+*/
 
-int matrix_multiply(Matrix *mat, Matrix *f_matrix, Matrix *s_matrix) {
+int matrix_multiply(int s_width, int f_height, int f_width, int result[f_height][s_width], int f_matrix[f_height][f_width], int s_matrix[f_width][s_width]) {
   /*faz a multiplicacao de duas matrizes*/
-
-  //pra multiplicar, a primeira matriz deve ter largura igual a altura a da segunda
-  if (f_matrix->width != s_matrix->height) {
-    return -1;
-  }
-
-  //a matriz resultado tem a altura da primeira matriz e a largura da segunda
-  if (mat->height != f_matrix->height || mat->width != s_matrix->width) {
-    return -1;
-  }
+  
 
   //o pivo é eh o f_col e o s_row, que sao os mesmos
   int pivo, f_row, s_col;
-  for(f_row = 0; f_row < f_matrix->height; f_row++) {
-    for(s_col = 0; s_col < s_matrix->width; s_col++) {
+  for(f_row = 0; f_row < f_height; f_row++) {
+    for(s_col = 0; s_col < s_width; s_col++) {
 
-      mat->mat[ matrix_index(f_row, s_col, mat->width) ] = 0;
-      for(pivo = 0; pivo < f_matrix->width; pivo++) {
+      result[f_row][s_col] = 0;
+      for(pivo = 0; pivo < f_width; pivo++) {
 
-        mat->mat[matrix_index(f_row, s_col, mat->width)] += f_matrix->mat[ matrix_index(f_row, pivo, f_matrix->width) ] * s_matrix->mat[ matrix_index(pivo, s_col, s_matrix->width) ];
+        result[f_row][s_col] += f_matrix[f_row][pivo] * s_matrix[pivo][s_col];
       }  
     }
   } 
@@ -91,7 +65,7 @@ int modular_inverse(int num, int base) {
   return -1;
 }
 
-int remove_spaces(char *text, char *dest, int text_size){
+int remove_spaces(char *dest, char *text, int text_size){
 
   int i, j;
   i = j = 0;
@@ -108,44 +82,44 @@ int remove_spaces(char *text, char *dest, int text_size){
   return i;
 }
 
-int encrypt(char *dest, char *text, int text_size, Matrix *key_matrix) {
+
+int fill_missing_spaces(char *text, int text_size){
+	
+	int space_to_fill = text_size % GAP;
+  	if(space_to_fill != 0){
+  	
+  		int i;
+  		for(i = 0; i < space_to_fill; i++){
+  			text[text_size + i] = 'a';
+		}
+	
+  	}
+  
+  	return text_size + space_to_fill;
+}
+ 
+
+int encrypt(char *dest, char *text, int text_size, int key_matrix[GAP][GAP]) {
   //encripta o texto usando uma matrix chave
 
-  //matrix chave tem que ser quadrada
-  if(key_matrix->width != key_matrix->height) {
-    return -1;
-  }
+  text_size = remove_blank_spaces(dest, text, text_size);
+  text = dest;
+  
+  text_size = fill_missing_spaces(text, text_size);
 
-  //must be par CHANGE IT
-  if(text_size%key_matrix->width != 0){
-  	
-  	int i;
-  	for(i = 0; i < text_size%key_matrix->width; i++){
-  		text[text_size + i] = 'a';
-	}
-	
-    text_size+= text_size%key_matrix->width;
-    
-    printf("%s - %d - \n", text, text_size);
-  }
-
-
-  int gap = key_matrix->width;
-  int gap_items[gap][1]; 
-  int mult_items[gap][1];
+  int gap_items[GAP][1]; 
+  int mult_items[GAP][1];
 
   int pivo, i;
-  for(pivo = 0; pivo < text_size; pivo += gap) {
-    for(i = pivo; i < pivo + gap; i++){ 
-      gap_items[i - pivo][0] = text[i] - 97; 
+  for(pivo = 0; pivo < text_size; pivo += GAP) {
+    for(i = pivo; i < pivo + GAP; i++){ 
+      gap_items[i - pivo][0] = char_to_number(text[i]);
     }
+	
+	matrix_multiply(1, GAP, GAP, mult_items, key_matrix, gap_items);	
 
-    Matrix gap_matrix = matrix_init(gap, 1, &gap_items[0][0]);
-    Matrix mult_matrix = matrix_init(gap, 1, &mult_items[0][0]);
-    matrix_multiply(&mult_matrix, key_matrix, &gap_matrix);
-
-    for(i = 0; i < gap; i++){
-      dest[pivo + i] = mult_matrix.mat[ matrix_index(i, 0, mult_matrix.width) ] % 26 + 97;
+    for(i = 0; i < GAP; i++){
+      dest[pivo + i] = number_to_char(mult_items[i][0] % 26);
     }
   }
   
@@ -156,18 +130,15 @@ int encrypt(char *dest, char *text, int text_size, Matrix *key_matrix) {
 
 int main(void) {
 
-  int matt[2][2] = {
+  int mat[GAP][GAP] = {
     {25, 22},
     {1, 23}
   };
-  Matrix mat = matrix_init(2, 2, &matt[0][0]);
 
   char dest[449];
   char text[449] = "o cuidado em identificar pontos criticos na valorizacao de fatores subjetivos causa impacto indireto na reavaliacao dos conhecimentos estrategicos para atingir a excelencia no mundo atual a hegemonia do ambiente politico agrega valor ao estabelecimento do processo de comunicacao como um todo por conseguinte a estrutura atual da organizacao acarreta um processo de reformulacao e modernizacao das condicoes financeiras e administrativas exigidas";
   
-  int size = remove_spaces(text, dest, 446);
-
-  encrypt(text, dest, size, &mat);
+  encrypt(dest, text, 446, mat);
 
   printf("%s", text);
   return 0;
