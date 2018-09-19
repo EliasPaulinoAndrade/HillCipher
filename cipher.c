9f5d18b24bda01c395ee8e04ed63453f656dec4b
EliasPaulinoAndrade <eliaspaulinoandrade@gmail.com>
2018-09-18 21:25:00 -0300

    begin add .

--- cipher.c
+++ cipher.c
@@ -3,11 +3,25 @@
 #include <string.h>
 
 #define GAP 2
-#define char_to_number(c) (c - 97)
-#define number_to_char(i) (i + 97)
+#define alphabet_size 27
 
-//CRIACAO DE STRING SEM /0
+int char_to_number(char c){
+	switch(c){
+		case '.':
+			return 26;
+		default:
+			return c - 97;
+	}
+}
 
+char number_to_char(int i){
+	switch(i){
+		case 26:
+			return '.';
+		default:
+			return i + 97;
+	}
+}
 
 void matrix_show(int width, int height, int matrix[height][width]){
   /*printa matrix*/
@@ -23,17 +37,9 @@ void matrix_show(int width, int height, int matrix[height][width]){
     	printf("\n");
   	}
 }
-/*
-Matrix *matrix_adjunt(Matrix *matrix) {
-  
-  
-  return NULL;
-}
-*/
 
 int matrix_multiply(int s_width, int f_height, int f_width, int result[f_height][s_width], int f_matrix[f_height][f_width], int s_matrix[f_width][s_width]) {
   	/*faz a multiplicacao de duas matrizes*/
-  
   
   	//o pivo é eh o f_col e o s_row, que sao os mesmos
   	int pivo, f_row, s_col;
@@ -103,13 +109,10 @@ int fill_missing_spaces(char *text, int text_size){
 int encrypt(char *dest, char *text, int text_size, int key_matrix[GAP][GAP]) {
   //encripta o texto usando uma matrix chave
 
-  	char *switch_text;
-  	text_size = remove_unwanted_words(dest, text, text_size);
-  	switch_text = text;
-  	text = dest;
-  	dest = switch_text;
+  	char accord_text[text_size];
+  	text_size = remove_unwanted_words(accord_text, text, text_size);
   
-  	text_size = fill_missing_spaces(text, text_size);
+  	text_size = fill_missing_spaces(accord_text, text_size);
 
   	int gap_items[GAP][1]; 
   	int mult_items[GAP][1];
@@ -117,22 +120,23 @@ int encrypt(char *dest, char *text, int text_size, int key_matrix[GAP][GAP]) {
   	int pivo, i;
   	for(pivo = 0; pivo < text_size; pivo += GAP) {
     	for(i = pivo; i < pivo + GAP; i++){ 
-      		gap_items[i - pivo][0] = char_to_number(text[i]);
+      		gap_items[i - pivo][0] = char_to_number(accord_text[i]);
     	}
 	
 		matrix_multiply(1, GAP, GAP, mult_items, key_matrix, gap_items);	
 
     	for(i = 0; i < GAP; i++){
-      		dest[pivo + i] = number_to_char(mult_items[i][0] % 26);
+      		dest[pivo + i] = number_to_char(mult_items[i][0] % alphabet_size);
    		}
   	}
   
   	dest[text_size] = '\0';
   
-  	return 1;  
+  	return text_size;  
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
+	
 	int encrypt_key[GAP][GAP] = {
     	{2, 3},
     	{5, 7}
@@ -140,15 +144,21 @@ int main(void) {
   	
   	int decrypt_key[GAP][GAP] = {
     	{19, 3},
-    	{5, }
+    	{5, 24}
   	};
-
-
-  	char dest[3067];
+  	
+  	
+	char dest[3067];
   	char text[3067] = "todas estas questoes devidamente ponderadas levantam duvidas sobre se a necessidade de renovacao processual ainda nao demonstrou convincentemente que vai participar na mudanca dos indices pretendidos o que temos que ter sempre em mente e que o julgamento imparcial das eventualidades estende o alcance e a importancia das diversas correntes de pensamento a nivel organizacional a execucao dos pontos do programa acarreta um processo de reformulacao e modernizacao das condicoes financeiras e administrativas exigidas acima de tudo e fundamental ressaltar que a valorizacao de fatores subjetivos desafia a capacidade de equalizacao dos paradigmas corporativos gostaria de enfatizar que o novo modelo estrutural aqui preconizado maximiza as possibilidades por conta das formas de acao todavia a continua expansao de nossa atividade causa impacto indireto na reavaliacao do processo de comunicacao como um todo e claro que a consulta aos diversos militantes pode nos levar a considerar a reestruturacao das condicoes inegavelmente apropriadas evidentemente a adocao de politicas descentralizadoras afeta positivamente a correta previsao dos metodos utilizados na avaliacao de resultados desta maneira a complexidade dos estudos efetuados representa uma abertura para a melhoria das direcoes preferenciais no sentido do progresso neste sentido o desafiador cenario globalizado oferece uma interessante oportunidade para verificacao das novas proposicoes no entanto nao podemos esquecer que o entendimento das metas propostas auxilia a preparacao e a composicao dos niveis de motivacao departamental nunca e demais lembrar o peso e o significado destes problemas uma vez que a revolucao dos costumes nao pode mais se dissociar do investimento em reciclagem tecnica a certificacao de metodologias que nos auxiliam a lidar com o aumento do dialogo entre os diferentes setores produtivos obstaculiza a apreciacao da importancia do levantamento das variaveis envolvidas percebemos cada vez mais que a constante divulgacao das informacoes deve passar por modificacoes independentemente das diretrizes de desenvolvimento para o futuro ainda assim existem duvidas a respeito de como o comprometimento entre as equipes apresenta tendencias no sentido de aprovar a manutencao de todos os recursos funcionais envolvidos por outro lado a hegemonia do ambiente politico nos obriga a analise do sistema de formacao de quadros que corresponde as necessidades no mundo atual a consolidacao das estruturas garante a contribuicao de um grupo importante na determinacao das posturas dos orgaos dirigentes com relacao as suas atribuicoes por conseguinte a mobilidade dos capitais internacionais e uma das consequencias dos conhecimentos estrategicos para atingir a excelencia o incentivo ao avanco tecnologico assim como o acompanhamento das preferencias de consumo assume importantes posicoes no estabelecimento do retorno esperado a longo prazo e importante questionar o quanto o consenso sobre a necessidade de qualificacao estimula a padronizacao do remanejamento dos quadros funcionais";
   
-  	encrypt(dest, text, 3066, encrypt_key);
-  	printf("%s", text);
+  	int size;
+  
+  	size = encrypt(dest, text, strlen(text), encrypt_key);
+  	printf("%s [SIZE: %d]\n\n", dest, size);
   	
+  	encrypt(text, dest, size, decrypt_key);
+  	printf("%s", text);
+	
+  
   	return 0;
 }
