4163f71b6e8a5dc5e5ab9999962ebde67fece868
elias paulino <eliaspaulinoandrade@gmail.com>
2018-09-20 21:35:10 -0300

    bug fill spaces, config file

--- cipherHill.c
+++ cipherHill.c
@@ -2,35 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "config.c"
 
 #define TRUE 1
 #define FALSE 0
 
-#define GAP 2
-#define ALPHABET_SIZE 30
-#define FILL_CHAR 'a'
-
-#define TEXT_EXAMPLE "Todas estas questoes, devidamente ponderadas, levantam duvidas sobre se a mobilidade dos capitais internacionais possibilita uma melhor visao global dos paradigmas corporativos. Acima de tudo, e fundamental ressaltar que a estrutura atual da organizacao pode nos levar a considerar a reestruturacao do retorno esperado a longo prazo. O incentivo ao avanco tecnologico, assim como a continua expansao de nossa atividade agrega valor ao estabelecimento do sistema de formacao de quadros que corresponde as necessidades. Caros amigos, o surgimento do comercio virtual oferece uma interessante oportunidade para verificacao do sistema de participacao geral. A nivel organizacional, o desenvolvimento continuo de distintas formas de atuacao afeta positivamente a correta previsao dos procedimentos normalmente adotados. \n\
-No entanto, nao podemos esquecer que o consenso sobre a necessidade de qualificacao exige a precisao e a definicao da gestao inovadora da qual fazemos parte. Podemos ja vislumbrar o modo pelo qual o comprometimento entre as equipes maximiza as possibilidades por conta do orcamento setorial. A certificacao de metodologias que nos auxiliam a lidar com o aumento do dialogo entre os diferentes setores produtivos nos obriga a analise de alternativas as solucoes ortodoxas. \n\
-Desta maneira, a valorizacao de fatores subjetivos e uma das consequencias dos metodos utilizados na avaliacao de resultados. Todavia, o julgamento imparcial das eventualidades estimula a padronizacao dos conhecimentos estrategicos para atingir a excelencia. Ainda assim, existem duvidas a respeito de como a complexidade dos estudos efetuados deve passar por modificacoes independentemente das direcoes preferenciais no sentido do progresso. \n\
-Neste sentido, o inicio da atividade geral de formacao de atitudes prepara-nos para enfrentar situacoes atipicas decorrentes das posturas dos orgaos dirigentes com relacao as suas atribuicoes. O que temos que ter sempre em mente e que o acompanhamento das preferencias de consumo representa uma abertura para a melhoria dos modos de operacao convencionais. Pensando mais a longo prazo, a consolidacao das estruturas causa impacto indireto na reavaliacao do levantamento das variaveis envolvidas. \n\
-E claro que a crescente influencia da midia talvez venha a ressaltar a relatividade dos relacionamentos verticais entre as hierarquias. As experiencias acumuladas demonstram que a expansao dos mercados mundiais acarreta um processo de reformulacao e modernizacao das diversas correntes de pensamento. Por outro lado, a hegemonia do ambiente politico auxilia a preparacao e a composicao das regras de conduta normativas. Nunca e demais lembrar o peso e o significado destes problemas, uma vez que o fenomeno da Internet nao pode mais se dissociar dos indices pretendidos. \n\
-Por conseguinte, a consulta aos diversos militantes desafia a capacidade de equalizacao do investimento em reciclagem tecnica. Do mesmo modo, o entendimento das metas propostas promove a alavancagem das formas de acao. No mundo atual, a determinacao clara de objetivos apresenta tendencias no sentido de aprovar a manutencao dos niveis de motivacao departamental."
-
-
-int encrypt_key[GAP][GAP] = {
-    {2,3},
-    {5,7}
-};
-
-int decrypt_key[GAP][GAP] = {
-    {23,3},
-    {5,28}
-};
+#define OK 0
+#define WRONG -1 
 
 int char_to_number(char c) {
     
-    int num = -1;
+    int num = WRONG;
     switch(c) {
 
 	case ' ':
@@ -52,7 +34,7 @@ int char_to_number(char c) {
     }
 
     if(num >= ALPHABET_SIZE) {
-        return -1;
+        return WRONG;
     }
     
     return num;
@@ -60,7 +42,7 @@ int char_to_number(char c) {
 
 char number_to_char(int i) {
 
-    char charc = -1;
+    char charc = WRONG;
     
     if(i >= ALPHABET_SIZE) {
         return charc;
@@ -122,7 +104,7 @@ int matrix_multiply(int s_width, int f_height, int f_width, int result[][s_width
             }
         }
     }
-    return 1;
+    return OK;
 }
 
 int modular_inverse(int num, int base){
@@ -136,13 +118,13 @@ int modular_inverse(int num, int base){
             return inverse;
         }
     }
-    return -1;
+    return WRONG;
 }
 
-void string_to_lower_case (char *text) {
+void string_to_lower_case (char *text, int text_size) {
 
     int i;
-    for (i = 0; i < strlen(text); i++){
+    for (i = 0; i < text_size; i++){
         text[i] = tolower(text[i]);
     }
 }
@@ -165,7 +147,7 @@ int remove_unwanted_words (char *dest, char *text, int text_size, char *exceptio
             }
         }
         
-        if (has_exceptions == FALSE && char_to_number(text[text_counter]) != -1){
+        if (has_exceptions == FALSE && char_to_number(text[text_counter]) != WRONG){
             dest[dest_counter] = text[text_counter];
             dest_counter++;
         }
@@ -196,7 +178,7 @@ int fill_missing_spaces(char *text, int text_size){
 
 int format_text(char *dest, char *text, int text_size) {
 
-    string_to_lower_case(text);
+    string_to_lower_case(text, text_size);
     text_size = remove_unwanted_words(dest, text, text_size, "");
     text_size = fill_missing_spaces(dest, text_size);
 
@@ -209,6 +191,8 @@ int encrypt(char *dest, char *text, int text_size, int key_matrix[][GAP], int wa
     char accord_text[text_size];
 
     text_size = format_text(accord_text, text, text_size);
+    
+    matrix_show(2, 2, key_matrix);
 
     int gap_items[GAP][1];
     int mult_items [GAP][1];
@@ -240,26 +224,31 @@ int write_to_file(char *text, char *file_path){
     FILE *file = fopen(file_path, "w");
     
     if(file == NULL) {
-        return -1;
+        return WRONG;
     }
     
     fprintf(file, "%s", text);
     fclose(file);
     
-    return 0;
+    return OK;
 }
 
-int read_from_file(char *dest, char *file_path) {
+int read_from_file(char *dest, int max_size, char *file_path) {
     FILE *file = fopen(file_path, "r");
     
     if(file == NULL) {
-        return -1;
+        return WRONG;
     }
     
-    fscanf(file, "%s", dest);
-    fclose(file);
+    char c, i = 0;
+    while ((c = getc(file)) != EOF && i < max_size - 1){
+        dest[i++] = c;
+    }
+    dest[i] = '\0';
     
-    return 0;
+    
+    fclose(file);
+    return OK;
 }
 
 int main (int argc, char *argv[]) {
@@ -282,7 +271,7 @@ int main (int argc, char *argv[]) {
         size = encrypt(dest, text, strlen(text), decrypt_key, 2);
         printf("%s", dest);
         
-	}else if(argc == 2 && strcmp(argv[1], "example") == 0) {
+    }else if(argc == 2 && strcmp(argv[1], "example") == 0) {
 		strcat(text, TEXT_EXAMPLE);
 
         size = encrypt(dest, text, strlen(text), encrypt_key, 1);
@@ -295,7 +284,7 @@ int main (int argc, char *argv[]) {
         strcat(text, argv[2]);
         
         size = encrypt(dest, text, strlen(text), encrypt_key, 1);
-        if(write_to_file(dest, argv[4]) == 0) {
+        if(write_to_file(dest, argv[4]) == OK) {
             printf("Done.\n");
         }else {
             printf("Error While Writing in File.\n");
@@ -303,7 +292,7 @@ int main (int argc, char *argv[]) {
     }else if(argc == 3 && strcmp(argv[1], "decFromFile") == 0){
         char file_text[4000];
         
-        if(read_from_file(file_text, argv[2]) == 0) {
+        if(read_from_file(file_text, 4000, argv[2]) == OK) {
 	     strcat(text, file_text);
              size = encrypt(dest, text, strlen(text), decrypt_key, 2);
         
